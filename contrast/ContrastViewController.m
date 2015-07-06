ad89b1849e10edc3caa9615d05f8af5fdbf14b0f
Johan Halin <johan@halin.me>
2015-07-06 16:10:58 +0300

    Added interaction for adding and removing channels. Double-tapping the white space adds a channel, while double-tapping a channel removes it.

--- contrast/ContrastViewController.m
+++ contrast/ContrastViewController.m
@@ -9,24 +9,97 @@
 #import "ContrastViewController.h"
 #import "ContrastChannelView.h"
 
-@interface ContrastViewController ()
+static const NSInteger ContrastMaximumChannelCount = 8;
 
+@interface ContrastViewController ()
+@property (nonatomic) NSMutableArray<ContrastChannelView *> *channels;
 @end
 
 @implementation ContrastViewController
+
+#pragma mark - Private
+
+- (void)_addChannelAtPoint:(CGPoint)point
+{
+	if (self.channels.count >= ContrastMaximumChannelCount)
+	{
+		// TODO: Maybe indicate somehow that more channels cannot be added?
+		
+		return;
+	}
+	
+	ContrastChannelView *channelView = [[ContrastChannelView alloc] initWithCenter:point];
+	channelView.alpha = 0;
+	channelView.transform = CGAffineTransformScale(CGAffineTransformIdentity, 0, 0);
+	[self.view addSubview:channelView];
+	[self.channels addObject:channelView];
+	
+	[UIView animateWithDuration:UINavigationControllerHideShowBarDuration delay:0 usingSpringWithDamping:0.7 initialSpringVelocity:0.5 options:0 animations:^{
+		channelView.alpha = 1;
+		channelView.transform = CGAffineTransformScale(CGAffineTransformIdentity, 1, 1);
+	} completion:nil];
+}
+
+- (void)_removeChannel:(ContrastChannelView *)channelView
+{
+	[UIView animateWithDuration:(UINavigationControllerHideShowBarDuration * 2.0) delay:0 usingSpringWithDamping:0.7 initialSpringVelocity:0.5 options:0 animations:^{
+		channelView.alpha = 0;
+		channelView.transform = CGAffineTransformRotate(channelView.transform, M_PI_4);
+		channelView.transform = CGAffineTransformScale(channelView.transform, 0.1, 0.1);
+	} completion:^(BOOL finished) {
+		[self.channels removeObject:channelView];
+		[channelView removeFromSuperview];
+	}];
+}
+
+- (void)_doubleTapRecognized:(UITapGestureRecognizer *)tapRecognizer
+{
+	CGPoint location = [tapRecognizer locationInView:self.view];
+	UIView *view = [self.view hitTest:location withEvent:nil];
+	
+	if (view == self.view)
+	{
+		[self _addChannelAtPoint:location];
+	}
+	else
+	{
+		UIView *baseView = [self _baseViewFromView:view];
+		
+		if ([baseView isKindOfClass:[ContrastChannelView class]])
+		{
+			[self _removeChannel:(ContrastChannelView *)baseView];
+		}
+	}
+}
+
+- (UIView *)_baseViewFromView:(UIView *)view
+{
+	if (view == self.view)
+	{
+		return nil;
+	}
+	
+	UIView *currentView = view;
+	
+	while (currentView.superview != self.view)
+	{
+		currentView = currentView.superview;
+	}
+	
+	return currentView;
+}
+
+#pragma mark - UIViewController
 
 - (void)viewDidLoad
 {
 	[super viewDidLoad];
 	
-	ContrastChannelView *channelView = [[ContrastChannelView alloc] initWithCenter:CGPointMake(100, 100)];
-	[self.view addSubview:channelView];
-
-	ContrastChannelView *channelView2 = [[ContrastChannelView alloc] initWithCenter:CGPointMake(200, 200)];
-	[self.view addSubview:channelView2];
-
-	ContrastChannelView *channelView3 = [[ContrastChannelView alloc] initWithCenter:CGPointMake(150, 300)];
-	[self.view addSubview:channelView3];
+	self.channels = [[NSMutableArray alloc] init];
+	
+	UITapGestureRecognizer *doubleTapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(_doubleTapRecognized:)];
+	doubleTapRecognizer.numberOfTapsRequired = 2;
+	[self.view addGestureRecognizer:doubleTapRecognizer];
 }
 
 - (BOOL)prefersStatusBarHidden
